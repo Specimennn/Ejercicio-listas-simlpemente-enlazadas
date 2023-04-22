360e30c67515bdc252c2895d9616956b22863b19
Alejandro Álvarez <118235591+Specimennn@users.noreply.github.com>
2023-04-21 23:04:42 -0500

    Add files via upload

---
+++ tads-master/tads-master/src/main/java/co/edu/umanizales/tads/controller/dto/KidsPerGenderDTO.java
@@ -0,0 +1,15 @@
+package co.edu.umanizales.tads.controller.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+@Data
+@AllArgsConstructor
+public class KidsPerGenderDTO {
+
+private char gender;
+
+private int quantity;
+
+}
---
+++ tads-master/tads-master/src/main/java/co/edu/umanizales/tads/model/Pet.java
@@ -0,0 +1,22 @@
+package co.edu.umanizales.tads.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Data
+public class Pet {
+
+    private String identification;
+
+    private String name;
+
+    private byte age;
+
+
+}
