9d5da776120c61ee883d5c98029df695e565a2ae
Alejandro Álvarez <118235591+Specimennn@users.noreply.github.com>
2023-05-05 22:37:29 -0500

    Add files via upload

---
+++ tads-master/tads-master/src/main/java/co/edu/umanizales/tads/controller/dto/ReportDTO.java
@@ -0,0 +1,17 @@
+package co.edu.umanizales.tads.controller.dto;
+
+import co.edu.umanizales.tads.model.Location;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ReportDTO {
+
+    private Location location;
+
+    private int total;
+
+    private KidsPerGenderDTO genders;
+
+}
---
+++ tads-master/tads-master/src/main/java/co/edu/umanizales/tads/service/AgeRangeService.java
@@ -0,0 +1,22 @@
+package co.edu.umanizales.tads.service;
+
+import co.edu.umanizales.tads.model.AgeRange;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Data
+public class AgeRangeService {
+
+    private List<AgeRange> ageRanges;
+
+    public AgeRangeService(){
+        ageRanges = new ArrayList<>();
+        ageRanges.add(new AgeRange(0,4));
+        ageRanges.add(new AgeRange(5,8));
+        ageRanges.add(new AgeRange(9,12));
+        ageRanges.add(new AgeRange(12,100));
+    }
+}
